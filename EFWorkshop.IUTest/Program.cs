57edc7161f3d7f52cf48378e90ce93ba8078f122
Kaszydo <kaszydo@gmail.com>
2023-09-23 16:30:48 +0200

    Update with what I missed

---
+++ EFWorkshop.IUTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using EFWorkshop.Data;
+using EFWorkshop.Domain;
+
+namespace EFWorkshop.IUTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var companyContext = new CompanyContext();
+            var employees = companyContext.Employees;
+
+            foreach (var employee in employees)
+            { 
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
